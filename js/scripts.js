d38940f287efa5abc912044ba24d963687c7fac2
njanus <60715561+njanus2001@users.noreply.github.com>
2022-03-21 01:05:02 -0700

    Created early version

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
-function test() {
-    console.log("Test")
-}
 
-test();
+
+function getWeather () {
+    let city = document.getElementById('city').value;
+
+    // API Key Removed
+    $.get("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=", function(data){
+        console.log(data);
+
+        document.forms['weather-form'].reset();
+        document.getElementById('city-text').innerHTML = data.name;
+        document.getElementById('weather-icon').src = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+        document.getElementById('icon-div').style.visibility = 'visible';
+        document.getElementById('temp').innerHTML = data.main.temp + "°C";
+    });
+}
